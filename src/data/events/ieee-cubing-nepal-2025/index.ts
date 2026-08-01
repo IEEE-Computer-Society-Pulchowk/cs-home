8d5ae228a702f3a90c85594d5ddf0a66a8f7ab13
Shubha Sandesh <indivshubha@gmail.com>
2026-08-01 19:28:11 +0545

    Add IEEE Cubing Nepal 2025 event

---
+++ src/data/events/ieee-cubing-nepal-2025/index.ts
@@ -0,0 +1,34 @@
+import { EventCategory } from "@/types";
+import type { EventRecord } from "../types";
+
+export const IEEECubingNepal2025: EventRecord = {
+  slug: "ieee-cubing-nepal-2025",
+  title: "IEEE Cubing Nepal 2025",
+  category: EventCategory.COMPETITION,
+  description:
+    "IEEE Cubing Nepal 2025, organized by IEEE Computer Society Pulchowk SBC, is a speedcubing competition featuring cubers and official cubing partners.",
+  thumbnail: "/events/2025/ieee-cubing-nepal-2025.jpg",
+  registrationUrl: "https://bit.ly/IEEECubing2025",
+  recurrence: "annual",
+  years: {
+    "2025": {
+      title: "IEEE Cubing Nepal 2025",
+      slogan: "An exciting speedcubing experience powered by top cubing brands.",
+
+      phases: [
+        {
+          phase: 1,
+          title: "Official Cubing Partner Announcement",
+          startDate: "June 06, 2025",
+          endDate: undefined,
+          startTime: undefined,
+          endTime: undefined,
+          location: "Pulchowk Campus",
+          body: "",
+          bodyFile: "./body.md",
+          registrationUrl: "https://bit.ly/IEEECubing2025",
+        },
+      ],
+    },
+  },
+};
